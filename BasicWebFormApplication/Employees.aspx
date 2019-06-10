9da4e9d5157e981e7d019645db61b6eaf0781e0a
SadanSyk <48682456+SadanSyk@users.noreply.github.com>
2019-06-10 11:59:46 +0300

    Add files via upload

---
+++ BasicWebFormApplication/Employees.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Employees.aspx.cs" Inherits="BasicWebFormApplication.Employees1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        
+        <div>
+            <asp:Label ID="Label3" runat="server" Text="Lütfen Kullanıcı Adı ve Şifresini giriniz." ForeColor="Red"></asp:Label>
+              <br /><br />
+            <asp:Label ID="Label1" runat="server" Text="Kullanıcı Adı :" Width="100px"></asp:Label>
+                 <asp:TextBox ID="txtKullaniciAdi" runat="server"></asp:TextBox>
+            <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ErrorMessage=" Kullanici Adi alanı boş geçilemez" ControlToValidate="txtKullaniciAdi" ></asp:RequiredFieldValidator>
+            <br /> <br />
+             <asp:Label ID="Label2" runat="server" Text="Şifre :" Width="100px"></asp:Label>
+            <asp:TextBox ID="txtPassword" runat="server"></asp:TextBox>
+               <asp:RequiredFieldValidator ID="RequiredFieldValidator2" runat="server" ErrorMessage=" Şifre alanı boş geçilemez" ControlToValidate="txtPassword" ></asp:RequiredFieldValidator>
+            <br /> <br />
+             <asp:Label ID="Label4" runat="server" Text=" " Width="100px"></asp:Label>
+            <asp:Button ID="btnSubmit" runat="server" Text="Giriş" OnClick="btnSubmit_Click"  Width="174px"/>
+            <br />
+              <hr/>
+          <asp:DropDownList ID="ddlCustomers" runat="server" OnSelectedIndexChanged="ddlCustomers_SelectedIndexChanged" AutoPostBack="true"></asp:DropDownList>
+        <hr />
+            <asp:GridView ID="gvCustomers" runat="server"></asp:GridView>
+            <hr />
+            <asp:Label ID="lblSonuc" runat="server" Text=" "></asp:Label>
+        </div>
+    </form>
+    
+</body>
+</html>
---
+++ BasicWebFormApplication/Employees.aspx.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace BasicWebFormApplication
+{
+    public partial class Employees1 : System.Web.UI.Page
+    {
+        NORTHWNDEntities db;
+
+        public Employees1()
+        {
+            db = new NORTHWNDEntities();
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            if (IsPostBack)
+            {
+                return;
+            }
+            
+            GetOrders();
+        }
+
+        void GetOrders(string customerID = " ")
+        {
+            if (customerID == " ")
+            {
+                
+            }
+            else
+            {
+
+                gvCustomers.DataSource = db.Orders.Where(a => a.CustomerID == customerID).ToList();
+            }
+
+            gvCustomers.DataBind();
+        }
+
+       
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            var calisanlar = db.Employees.Select(a => new
+            {
+                KullaniciAdi = a.FirstName + "." + a.LastName,
+                Sifre=a.LastName+"."+a.BirthDate.Value.Year
+            }).ToList();
+
+            
+
+            foreach (var item in calisanlar)
+            {
+                if (txtKullaniciAdi.Text==item.KullaniciAdi)
+                {
+                    if (txtPassword.Text==item.Sifre)
+                    {
+                        ddlCustomers.DataSource = db.Customers.ToList();
+                        ddlCustomers.DataTextField = "CompanyName";
+                        ddlCustomers.DataValueField = "CustomerID";
+                        ddlCustomers.DataBind();
+                        return;
+                    }
+                    else
+                    {
+                        lblSonuc.Text = "şifre hatalı";
+                        return;
+                    }
+
+                }
+                else
+                {
+                    lblSonuc.Text = "Kullanıcı Adı Hatalı";
+                    return;
+                }
+            }
+                
+        }
+
+        protected void ddlCustomers_SelectedIndexChanged(object sender, EventArgs e)
+        {
+          string customerID = ddlCustomers.SelectedValue.ToString();
+                GetOrders(customerID);
+           
+        }
+    }
+}
